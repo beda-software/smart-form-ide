173dc59a71f867f623493618b0e6aad075515080
Ilya Beda <ir4y.ix@gmail.com>
2024-07-15 12:33:01 +1000

    WIP populate

--- src/AEHRCFormWrapper.tsx
+++ src/AEHRCFormWrapper.tsx
@@ -7,7 +7,7 @@ import {
 } from '@aehrc/smart-forms-renderer';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Questionnaire, QuestionnaireResponse } from 'fhir/r4';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { RenderRemoteData } from 'web/src/components/RenderRemoteData';
 
 import { sequenceMap } from '@beda.software/remote-data';
@@ -16,22 +16,29 @@ import { formatError } from '@beda.software/fhir-react';
 
 import { useDebounce } from './utils';
 import { QRFormWrapperProps } from "web/src/containers/Main/types";
+import { Parameters } from 'fhir/r4b';
 
 
 interface YourBaseRendererWrapperProps {
     questionnaire: Questionnaire;
     questionnaireResponse: QuestionnaireResponse;
     saveQuestionnaireResponse: (resource: QuestionnaireResponse) => void;
+    launchContextParameters: Parameters['parameter'];
 }
 
-function YourBaseRendererWrapper(props: YourBaseRendererWrapperProps) {
-    const { questionnaire, questionnaireResponse, saveQuestionnaireResponse } = props;
-
+function YourBaseRendererWrapper({ questionnaire, questionnaireResponse, saveQuestionnaireResponse, launchContextParameters }: YourBaseRendererWrapperProps) {
     // The renderer needs a query client to make API calls
     const queryClient = useRendererQueryClient();
 
+    const additionalVars: Record<string, object> = useMemo(() => {
+        let result: Record<string, object> = {};
+        launchContextParameters.forEach((p) => {
+            result[p.name!] = p.resource!
+        })
+        return result;
+    }, [launchContextParameters])
     // This hook builds the form based on the questionnaire
-    const isBuilding = useBuildForm(questionnaire, questionnaireResponse);
+    const isBuilding = useBuildForm(questionnaire, questionnaireResponse, false, undefined, additionalVars);
     const formQR = useDebounce(useQuestionnaireResponseStore.use.updatableResponse(), 500)
 
     useEffect(() => {
@@ -55,6 +62,7 @@ export function AEHRCFormWrapper({
     questionnaireRD,
     questionnaireResponseRD,
     saveQuestionnaireResponse,
+    launchContextParameters,
 }: QRFormWrapperProps) {
     const remoteDataResult = sequenceMap({
         questionnaireRD,
@@ -73,6 +81,7 @@ export function AEHRCFormWrapper({
                     questionnaire={data.questionnaireRD as Questionnaire}
                     questionnaireResponse={data.questionnaireResponseRD as QuestionnaireResponse}
                     saveQuestionnaireResponse={saveQuestionnaireResponse as YourBaseRendererWrapperProps['saveQuestionnaireResponse']}
+                    launchContextParameters={launchContextParameters}
                 />
             )}
         </RenderRemoteData>);
--- src/sdc.ts
+++ src/sdc.ts
@@ -1,13 +1,38 @@
-import { success } from "@beda.software/remote-data";
-import { Parameters, Questionnaire, QuestionnaireResponse } from "fhir/r4b";
+import { isFailure, isSuccess, success } from "@beda.software/remote-data";
+import { Parameters, Questionnaire, QuestionnaireResponse, OperationOutcome, Patient, Practitioner, Encounter} from "fhir/r4b";
 import { service } from "web/src/services/fhir";
+import {populateQuestionnaire } from "@aehrc/sdc-populate"
+import { extractErrorDescription } from "@beda.software/fhir-react";
 
-export function populate(_launchContext:Parameters) {
-    const result: QuestionnaireResponse = {
-        resourceType: "QuestionnaireResponse",
-        status: 'in-progress',
+async function fetchResource(query: string, _requestConfig?: any){
+    const response = await service({url: query})
+    console.log(response)
+
+    if(isSuccess(response)){
+        return response.data;
+    } else if(isFailure(response)) {
+        throw new Error(extractErrorDescription(response.error))
     }
-    return Promise.resolve(success(result));
+}
+
+function getParam(parameters: Parameters, name: string){
+    const param = parameters.parameter?.find(p => p.name === name);
+    if(param){
+        return param.resource;
+    }
+}
+
+export async function populate(launchContext: Parameters) {
+    const questionnaire: Questionnaire = getParam(launchContext, "questionnaire")! as any as Questionnaire;
+    const patient: Patient = getParam(launchContext, "patient")! as Patient;
+    const user: Practitioner = getParam(launchContext, "user")! as Practitioner;
+    const encounter: Encounter = getParam(launchContext, "encounter")! as Encounter;
+    const {populateResult } = await populateQuestionnaire({ questionnaire, fetchResourceCallback: fetchResource, patient, user, encounter });
+    if(populateResult.issues){
+        console.warn(JSON.stringify(populateResult.issues))
+    }
+    // console.log("population result ", populateResult.populatedResponse)
+    return success(populateResult.populatedResponse);
 }
 
 export async function assemble(questionnaireId:string){
